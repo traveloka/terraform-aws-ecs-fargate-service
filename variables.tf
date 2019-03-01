6cff1923e3e73ce2d79b3b67a463a5fa5ca950c4
Abi Hafshin <abi@hafs.in>
2019-03-01 17:19:22 +0700

    reorder variables param

--- variables.tf
+++ variables.tf
@@ -26,13 +26,13 @@ variable "target_group" {
 }
 
 variable "image_name" {
-  type        = "string"
   description = "The name of docker image that will be used by this service"
+  type        = "string"
 }
 
 variable "service_version" {
-  type        = "string"
   description = "The tag of the docker version to run "
+  type        = "string"
 }
 
 variable "main_container_name" {
@@ -60,52 +60,52 @@ variable "security_groups" {
 variable "assign_public_ip" {
   description = "Assign a public IP address to the ENI"
   type        = "string"
-  default     = "false"
+  default     = false
 }
 
 variable "log_group_name" {
-  type        = "string"
   description = "CloudWatch log group name where the service log place"
+  type        = "string"
 }
 
 variable "log_retention" {
+  description = "The number of day the logs will be keept"
   type        = "string"
   default     = 30
-  description = "The number of day the logs will be keept"
 }
 
 variable "container_definition_template" {
+  description = "Custom container definition template"
   type        = "string"
   default     = ""
-  description = "Custom container definition template"
 }
 
 variable "task_role" {
+  description = "The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
   type        = "string"
   default     = ""
-  description = "The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
 }
 
 variable "execution_role_name" {
+  description = "The name of the execution role that will be used by fargate to run tasks"
   type        = "string"
   default     = "ecsTaskExecutionRole"
-  description = "The name of the execution role that will be used by fargate to run tasks"
 }
 
 variable "environment" {
+  description = "Environment variables for the task"
   type        = "list"
   default     = []
-  description = "Environment variables for the task"
 }
 
 variable "cpu" {
   description = "The number of cpu units used by the task."
-  default     = "1024"
   type        = "string"
+  default     = "1024"
 }
 
 variable "memory" {
   description = "The amount (in MiB) of memory used by the task."
-  default     = "1024"
   type        = "string"
+  default     = "1024"
 }
