37aa4d09e92b47344ded5865f87c73d9a7380875
Abi Hafshin <abi@hafs.in>
2020-11-01 18:48:35 +0700

    Support terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "service_name" {
-  value       = "${aws_ecs_service.ecs_service.name}"
+  value       = aws_ecs_service.ecs_service.name
   description = "The name of the ECS service"
 }
 
 output "service_arn" {
-  value       = "${aws_ecs_service.ecs_service.id}"
+  value       = aws_ecs_service.ecs_service.id
   description = "The ARN of the ECS service"
 }
 
 output "taskdef_family" {
-  value       = "${aws_ecs_task_definition.task_def.family}"
+  value       = aws_ecs_task_definition.task_def.family
   description = "The family name of the task definition"
 }
 
 output "taskdef_arn" {
-  value       = "${aws_ecs_task_definition.task_def.arn}"
+  value       = aws_ecs_task_definition.task_def.arn
   description = "The full ARN of the task definition"
 }
