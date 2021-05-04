995d3902ddf06b911b6b8b0f98628852d6f1f99b
Abi Hafshin Alfarouq <abi.alfarouq@traveloka.com>
2021-05-04 11:16:27 +0700

    Support terraform 0.12 (#9)
    Co-authored-by: Ayush Choubey <ayush.choubey@traveloka.com>

--- main.tf
+++ main.tf
@@ -2,125 +2,123 @@ locals {
   cluster = "${var.service_name}-${var.cluster_role}"
 
   global_tags = {
-    Service       = "${var.service_name}"
-    Cluster       = "${local.cluster}"
-    Application   = "${var.application}"
-    ProductDomain = "${var.product_domain}"
-    Environment   = "${var.environment}"
+    Service       = var.service_name
+    Cluster       = local.cluster
+    Application   = var.application
+    ProductDomain = var.product_domain
+    Environment   = var.environment
     ManagedBy     = "Terraform"
   }
 
   service_tags = {
-    Name = "${module.service_name.name}"
+    Name = module.service_name.name
   }
 
   taskdef_tags = {
-    Name = "${module.taskdef_name.name}"
+    Name = module.taskdef_name.name
   }
+
+  container_definitions = templatefile(var.container_definition_template_file != "" ? var.container_definition_template_file : "${path.module}/templates/container-definition.json.tpl", {
+    aws_region     = data.aws_region.current.name
+    container_name = var.main_container_name
+    image_name     = var.image_name
+    version        = var.image_version
+    port           = var.main_container_port
+    log_group      = aws_cloudwatch_log_group.log_group.name
+    environment    = jsonencode(var.environment_variables)
+    product_domain = var.product_domain
+  })
 }
 
 module "service_name" {
-  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.11.0"
+  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.19.1"
 
-  name_prefix   = "${local.cluster}"
+  name_prefix   = local.cluster
   resource_type = "ecs_service"
 }
 
 module "taskdef_name" {
-  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.11.0"
+  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.19.1"
 
-  name_prefix   = "${local.cluster}"
+  name_prefix   = local.cluster
   resource_type = "ecs_task_definition"
 }
 
 module "log_group_name" {
-  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.11.0"
+  source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.19.1"
 
   name_prefix   = "/tvlk/${var.cluster_role}-${var.application}/${var.service_name}"
   resource_type = "cloudwatch_log_group"
 }
 
 resource "aws_ecs_service" "ecs_service" {
-  name          = "${module.service_name.name}"
-  cluster       = "${var.ecs_cluster_arn}"
-  desired_count = "${var.capacity}"
+  name          = module.service_name.name
+  cluster       = var.ecs_cluster_arn
+  desired_count = var.capacity
 
-  # Hack, so we can fill it with empty "" to prevent destroying
-  # resources with capacity_provider_strategy enabled via web console.
-  # capacity_provider_strategy is only available in aws provider > 2.42.0
-  launch_type = "${var.launch_type}"
+  launch_type = length(var.capacity_provider_strategies) > 0 ? null : "FARGATE"
 
-  platform_version = "${var.platform_version}"
+  dynamic "capacity_provider_strategy" {
+    for_each = var.capacity_provider_strategies
+    content {
+      capacity_provider = capacity_provider_strategy.value.capacity_provider
+      base              = capacity_provider_strategy.value.base
+      weight            = capacity_provider_strategy.value.weight
+    }
+  }
 
-  # Track the latest ACTIVE revision
-  task_definition = "${aws_ecs_task_definition.task_def.family}:${max("${aws_ecs_task_definition.task_def.revision}", "${data.aws_ecs_task_definition.task_def_data.revision}")}"
+  platform_version = var.platform_version
 
-  health_check_grace_period_seconds = "${var.health_check_grace_period_seconds}"
+  task_definition = aws_ecs_task_definition.task_def.family
+
+  health_check_grace_period_seconds = var.health_check_grace_period_seconds
 
   network_configuration {
-    subnets          = ["${var.subnet_ids}"]
-    security_groups  = ["${var.security_group_ids}"]
-    assign_public_ip = "${var.assign_public_ip}"
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = var.assign_public_ip
   }
 
   load_balancer {
-    target_group_arn = "${var.target_group_arn}"
-    container_name   = "${var.main_container_name}"
-    container_port   = "${var.main_container_port}"
+    target_group_arn = var.target_group_arn
+    container_name   = var.main_container_name
+    container_port   = var.main_container_port
   }
 
   propagate_tags = "SERVICE"
-  tags           = "${merge(local.global_tags, local.service_tags, var.service_tags)}"
+  tags           = merge(local.global_tags, local.service_tags, var.service_tags)
 
   lifecycle {
     ignore_changes = [
-      "desired_count",
+      desired_count,
+      launch_type, // somehow it conflict with capacity provider
+      task_definition, // https://github.com/terraform-providers/terraform-provider-aws/issues/632
     ]
   }
 }
 
 resource "aws_ecs_task_definition" "task_def" {
-  family                = "${module.taskdef_name.name}"
-  container_definitions = "${data.template_file.container_definition.rendered}"
-  task_role_arn         = "${var.task_role_arn}"
+  family                = module.taskdef_name.name
+  container_definitions = local.container_definitions
+  task_role_arn         = var.task_role_arn
 
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = "${var.execution_role_arn}"
+  execution_role_arn       = var.execution_role_arn
   network_mode             = "awsvpc"
 
-  cpu    = "${var.cpu}"
-  memory = "${var.memory}"
+  cpu    = var.cpu
+  memory = var.memory
 
-  tags = "${merge(local.global_tags, local.taskdef_tags, var.taskdef_tags)}"
+  tags = merge(local.global_tags, local.taskdef_tags, var.taskdef_tags)
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-data "template_file" "container_definition" {
-  template = "${var.container_definition_template != "" ? var.container_definition_template : file("${path.module}/templates/container-definition.json.tpl")}"
-
-  vars {
-    aws_region     = "${data.aws_region.current.name}"
-    container_name = "${var.main_container_name}"
-    image_name     = "${var.image_name}"
-    version        = "${var.image_version}"
-    port           = "${var.main_container_port}"
-    log_group      = "${aws_cloudwatch_log_group.log_group.name}"
-    environment    = "${jsonencode(var.environment_variables)}"
-    product_domain = "${var.product_domain}"
-  }
-}
-
 resource "aws_cloudwatch_log_group" "log_group" {
-  name              = "${module.log_group_name.name}"
-  retention_in_days = "${var.log_retention_in_days}"
+  name              = module.log_group_name.name
+  retention_in_days = var.log_retention_in_days
 
-  tags = "${merge(local.global_tags, var.log_tags)}"
-}
-
-data "aws_ecs_task_definition" "task_def_data" {
-  depends_on      = ["aws_ecs_task_definition.task_def"]
-  task_definition = "${aws_ecs_task_definition.task_def.family}"
+  tags = merge(local.global_tags, var.log_tags)
 }
--- variables.tf
+++ variables.tf
@@ -1,156 +1,169 @@
 variable "service_name" {
   description = "Name of the ECS service. Note: this value will be also used to name resources"
-  type        = "string"
+  type        = string
 }
 
 variable "cluster_role" {
   description = "The role of the cluster in the service"
-  type        = "string"
+  type        = string
+  default     = "app"
 }
 
 variable "application" {
-  description = "Application type that the ASG's instances will serve"
-  type        = "string"
+  description = "Application type that the ECS task will serve"
+  type        = string
 }
 
 variable "product_domain" {
   description = "The product domain that this service belongs to"
-  type        = "string"
+  type        = string
 }
 
 variable "environment" {
   description = "Environment where the service run"
-  type        = "string"
-  default     = "development"
+  type        = string
 }
 
 variable "ecs_cluster_arn" {
   description = "ARN of the ECS cluster to launch service in"
-  type        = "string"
+  type        = string
 }
 
 variable "platform_version" {
   description = "Version of the Fargate platform to run the service on"
-  type        = "string"
+  type        = string
   default     = "LATEST"
 }
 
 variable "capacity" {
   description = "Number of tasks to run in the service"
-  type        = "string"
+  type        = string
   default     = 2
 }
 
 variable "target_group_arn" {
   description = "ARN of the ALB target group to associate with the service"
-  type        = "string"
+  type        = string
 }
 
 variable "image_name" {
   description = "Name of the docker image that will be used by the task"
-  type        = "string"
+  type        = string
 }
 
 variable "image_version" {
   description = "Version/tag of the docker image to be used by the task"
-  type        = "string"
+  type        = string
+}
+
+variable "use_latest_task_definition" {
+  description = "Whether to always use latest task definition. Set it to false if you have external release tool"
+  type        = bool
+  default     = true
 }
 
 variable "main_container_name" {
   description = "Name of the container name that will be registered to target group"
-  type        = "string"
+  type        = string
   default     = "app"
 }
 
 variable "main_container_port" {
   description = "Port for main container to listen for incoming connections"
-  type        = "string"
+  type        = string
   default     = 80
 }
 
 variable "health_check_grace_period_seconds" {
   description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown."
-  type        = "string"
+  type        = string
   default     = 0
 }
 
 variable "subnet_ids" {
   description = "List of IDs of subnets to launch the service in"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "security_group_ids" {
   description = "List of IDs of security groups to associate with the service"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "assign_public_ip" {
   description = "Whether or not to assign public IP address to the task ENI"
-  type        = "string"
+  type        = string
   default     = false
 }
 
 variable "log_retention_in_days" {
   description = "Number of days to retain service logs"
-  type        = "string"
+  type        = string
   default     = 14
 }
 
-variable "container_definition_template" {
+variable "container_definition_template_file" {
   description = "Custom container definition template"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "task_role_arn" {
   description = "ARN of IAM role to be used by the task"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "execution_role_arn" {
   description = "ARN of IAM role to be used by container agent to pull container images, write logs, access ECS secrets and parameter store"
-  type        = "string"
+  type        = string
 }
 
 variable "environment_variables" {
   description = "List of environment variables to pass to the task"
-  type        = "list"
-  default     = []
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  default = []
 }
 
 variable "cpu" {
   description = "Number of cpu units to allocate for one task"
-  type        = "string"
-  default     = "1024"
+  type        = number
+  default     = 1024
 }
 
 variable "memory" {
   description = "Amount of memory (in MiB) to allocate for one task"
-  type        = "string"
+  type        = number
   default     = "1024"
 }
 
 variable "service_tags" {
   description = "Custom tags for ECS Service"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "taskdef_tags" {
   description = "Custom tags for ECS Task Definition"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "log_tags" {
   description = "Custom tags for CloudWatch Log Group"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
-variable "launch_type" {
-  description = "The launch type on which to run your service. The valid values are \"\", EC2 and FARGATE"
-  type        = "string"
-  default     = "FARGATE"
+variable "capacity_provider_strategies" {
+  description = "The capacity provider strategy to use for the service."
+  type = list(object({
+    capacity_provider = string
+    weight            = number
+    base              = number
+  }))
+  default = []
 }
